8d1f0fadefec7d2482d13f45440b29fb41cf2af7
Angel Komarov <angelkomarov@hotmail.com>
2025-08-18 16:32:25 +1000

    Base URL + headers are configured once in DI (not on every request). IHttpClientFactory manages connection pooling. Read settings from config file.

--- CurrencyConverter.Api/Program.cs
+++ CurrencyConverter.Api/Program.cs
@@ -1,15 +1,24 @@
-using CurrencyConverter.Api.Services.Interfaces;
+using CurrencyConverter.Api.Models;
 using CurrencyConverter.Api.Services;
+using CurrencyConverter.Api.Services.Interfaces;
+using System.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient();
+builder.Services.Configure<ProgramSettings>(builder.Configuration.GetSection("ProgramSettings"));
+builder.Services.AddHttpClient("ExchangeRateApi", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ProgramSettings:ExchangeRateUrl"]??"");
+    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped(typeof(ICurrencyConverterService), typeof(CurrencyConverterService));
 
 var app = builder.Build();
